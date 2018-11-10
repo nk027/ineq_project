dc3dc9d162c2281bd56e118639c123267037ed58
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2018-11-10 17:29:16 +0100

    Rework Casper's Laeken script to fit structure

--- R/_setup.R
+++ R/_setup.R
@@ -1,13 +1,10 @@
 # Setup -------------------------------------------------------------------
 
-issue_1_dealt_with <- FALSE
-country <- "AT"
-year <- 2013
-
-
-# Dependencies ------------------------------------------------------------
-
 library(dplyr)
+issue_1_dealt_with <- FALSE
+if(!exists(c("country", "year"))) {
+  stop("Please specify country and year.")
+}
 
 
 # Prepare Data ------------------------------------------------------------
@@ -24,19 +21,19 @@ if(issue_1_dealt_with) {
 
 # Download data
 silc.p <- tbl(pg, "pp") %>%
-  filter(pb020 %in% country & pb010 == year) %>%
-  select(pb020, pb030, pb040, py010g, px030) %>%
+  filter(pb020 %in% country & pb010 %in% year) %>%
+  select(pb020, pb030, pb040, pb150, py010g, px010, px030, pl060, pl073, pl074, pl100, rx060) %>%
   collect(n = Inf)
 
 silc.h <- tbl(pg, "hh") %>%
-  filter(hb020 %in% country & hb010 == year) %>%
+  filter(hb020 %in% country & hb010 %in% year) %>%
   select(hb020, hb030, hy010, hx010) %>%
-  collect()
+  collect(n = Inf)
 
 silc.d <- tbl(pg, "dd") %>%
-  filter(db020 %in% country & db010 == year) %>%
+  filter(db020 %in% country & db010 %in% year) %>%
   select(db010, db020, db030, db040, db090, px010) %>%
-  collect()
+  collect(n = Inf)
 
 # Create unique IDs for merging
 silc.p <- silc.p %>% mutate(id_h = paste0(pb020, px030))
---
+++ reports/CZE/laeken_indicators.R
@@ -0,0 +1,50 @@
+# ------------------------------------------------------------------------
+#
+# Laeken Indicators
+# Autoren: Engelen & Kuschnig
+# Datum: 2018-11-10
+#
+# -------------------------------------------------------------------------
+
+library(laeken)
+library(dplyr)
+
+country <- "CZ"
+year <- 2013
+
+# Source the Setup script to provide merged household and personal data
+source("R/_setup.R")
+
+
+# Subsetting --------------------------------------------------------------
+
+# To get useful results we may want to subset to only positive income
+silc.pd.inc <- silc.pd %>% filter(py010g > 0)
+silc.hd.inc <- silc.hd %>% filter(hy010 > 0)
+
+# For hourly wages we replace NAs in working hours with 0
+silc.pd.wage <- silc.pd
+silc.pd.wage$pl060[is.na(silc.pd.wage$pl060)] <- 0
+silc.pd.wage$pl100[is.na(silc.pd.wage$pl100)] <- 0
+# Filter out observations with no income and/or hours worked
+silc.pd.wage <- silc.pd.wage %>% 
+  filter(py010g > 0 & pl060 > 0 & (pl073 + pl074) > 0)
+
+
+# Indicators --------------------------------------------------------------
+
+# Share of population at risk of poverty
+
+arpr(inc = silc.pd.inc$py010g, weights = silc.pd.inc$pb040, 
+     breakdown = silc.pd.inc$pb020)
+arpr(inc = silc.hd.inc$hy010, silc.hd.inc$db090)
+
+# Gender Pay Gap
+
+silc.pd.wage <- silc.pd.wage %>% 
+  mutate(hwages = py010g / ((pl060 + pl100) * (pl073 + pl074) * 52 / 12),
+         gender = factor(pb150, labels = c("Male", "Female")))
+
+silc.pd.wage <- silc.pd.wage %>% # Make sure female is the first level
+  mutate(gender = relevel(gender, "Female"))
+gpg(silc.pd.wage$hwages, gender = silc.pd.wage$gender)
