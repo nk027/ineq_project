fde0628bb0261b15325f03127974d20b436d701d
Casputïn <cengelen0@gmail.com>
2018-11-11 11:25:31 +0100

    exchanged gross income from employment(py010g) for total personal income, for all indicators

--- R/indicators_base.R
+++ R/indicators_base.R
@@ -23,7 +23,6 @@ source("R/_setup.R")
 silc.pd.inc <- silc.pd %>% filter(py010g > 0)
 silc.hd.inc <- silc.hd %>% filter(hy010 > 0)
 
-
 # Creating Survey Objects -------------------------------------------------
 
 silc.pd.svy <- svydesign(ids =  ~ id_h,
@@ -41,64 +40,64 @@ silc.hd.svy <- svydesign(ids = ~id_h,
 
 # Mean Income
 #
-svymean(~py010g, silc.pd.svy)
+svymean(~total.inc, silc.pd.svy)
 svymean(~hy010, silc.hd.svy)
 # For comparing countries
-# svyby(~py010g, ~as.factor(db020), silc.pd.svy, svymean)
+# svyby(~total.inc, ~as.factor(db020), silc.pd.svy, svymean)
 # svyby(~hy010, ~as.factor(db020), silc.hd.svy, svymean)
 
 # Median Income
 #
-svyquantile(~py010g, silc.pd.svy, quantiles = c(0.5))
+svyquantile(~total.inc, silc.pd.svy, quantiles = c(0.5))
 svyquantile(~hy010, silc.hd.svy, quantiles = c(0.5))
 
 # For comparing countries
-# svyby(~py010g, ~as.factor(db020), silc.pd.svy,
-#       svyquantile, ~py010g, quantiles = c(0.5), keep.var = FALSE)
+# svyby(~total.inc, ~as.factor(db020), silc.pd.svy,
+#       svyquantile, ~total.inc, quantiles = c(0.5), keep.var = FALSE)
 # svyby(~hy010, ~as.factor(db020), silc.hd.svy,
 #       svyquantile, ~hy010, quantiles = c(0.5), keep.var = FALSE)
 
 # Decile Points
 #
-svyquantile(~py010g, silc.pd.svy, quantiles = seq(0, 1, 0.1))
+svyquantile(~total.inc, silc.pd.svy, quantiles = seq(0, 1, 0.1))
 svyquantile(~hy010, silc.hd.svy, quantiles = seq(0, 1, 0.1))
 # For comparing countries
-# svyby(~py010g, ~as.factor(db020), silc.pd.svy, 
-#       svyquantile, ~py010g, quantiles = seq(0, 1, 0.1), keep.var = FALSE)
+# svyby(~total.inc, ~as.factor(db020), silc.pd.svy, 
+#       svyquantile, ~total.inc, quantiles = seq(0, 1, 0.1), keep.var = FALSE)
 # svyby(~hy010, ~as.factor(hb020), silc.pd.svy, 
-#       svyquantile, ~py010g, quantiles = seq(0, 1, 0.1), keep.var = FALSE)
+#       svyquantile, ~total.inc, quantiles = seq(0, 1, 0.1), keep.var = FALSE)
 
 # Quantile Share Ratio
 #
-svyqsr(~py010g, silc.pd.svy, 0.2, 0.8)
+svyqsr(~total.inc, silc.pd.svy, 0.2, 0.8)
 svyqsr(~hy010, silc.hd.svy, 0.2, 0.8)
 # For comparing countries
-# svyby(~py010g, ~as.factor(db020), silc.pd.svy, svyqsr, 0.2, 0.8)
+# svyby(~total.inc, ~as.factor(db020), silc.pd.svy, svyqsr, 0.2, 0.8)
 # svyby(~hy010, ~as.factor(db020), silc.hd.svy, svyqsr, 0.2, 0.8)
 
 # Top 10% Income Share
 #
-svytotal(~py010g, subset(silc.pd.svy, pb020 == country & py010g >= 
-                           as.numeric(svyquantile(~py010g, silc.pd.svy, quantile = 0.9)))) / 
-  svytotal(~py010g, subset(silc.pd.svy, pb020 == country))
+svytotal(~total.inc, subset(silc.pd.svy, pb020 == country & total.inc >= 
+                           as.numeric(svyquantile(~total.inc, silc.pd.svy, quantile = 0.9)))) / 
+  svytotal(~total.inc, subset(silc.pd.svy, pb020 == country))
 svytotal(~hy010, subset(silc.hd.svy, db020 == country & hy010 >= 
                           as.numeric(svyquantile(~hy010, silc.hd.svy, quantile = 0.9)))) /
   svytotal(~hy010,subset(silc.hd.svy, db020 == country))
 
 # Gini Coefficient
 #
-svygini(~py010g, silc.pd.svy)
+svygini(~total.inc, silc.pd.svy)
 svygini(~hy010, silc.hd.svy)
 # For comparing countries
-# svyby(~py010g, ~as.factor(db020), silc.pd.svy, svygini)
+# svyby(~total.inc, ~as.factor(db020), silc.pd.svy, svygini)
 # svyby(~hy010, ~as.factor(db020), silc.hd.svy, svygini)
 
 # Theil Index
 #
-svygei(~py010g, silc.pd.svy, epsilon = 1)
+svygei(~total.inc, silc.pd.svy, epsilon = 1)
 svygei(~hy010, silc.hd.svy, epsilon = 1)
 # For comparing countries
-# svyby(~py010g, ~as.factor(db020), silc.pd.svy,
+# svyby(~total.inc, ~as.factor(db020), silc.pd.svy,
 #      svygei, epsilon = 1)
 # svyby(~hy010, ~as.factor(db020), silc.hd.svy,
 #      svygei, epsilon = 1)
