b6cc0e5e67d82dd7d5d4f33ffdd33bc2349e1427
Nikolas K <31620977+nk027@users.noreply.github.com>
2018-11-15 13:08:03 +0100

    Allow password to be read from environment

--- R/_connection.R
+++ R/_connection.R
@@ -3,10 +3,11 @@
 library(dplyr)
 
 if(!exists("pg")) {
-
+  
+  if(!exists("password")) {password <- readline("Password: ")}
   pg <- src_postgres(dbname = "datacube", host = "ineq.wu.ac.at",
                      user = "lvineq", 
-                     password = readline("Password: "), 
+                     password = password, 
                      options = "-c search_path=silc")
 } else {
   message("Connection pg already exists.")
